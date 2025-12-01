4031f496cac11ed48db640042f258a01b020ebaf
N101033 <n100431@N101033.local>
2025-12-01 10:41:41 +0300

    Initial commit - Trello API Automation Project

--- src/test/java/com/testinium/trello/runners/TestRunner.java
+++ src/test/java/com/testinium/trello/runners/TestRunner.java
@@ -1,3 +1,14 @@
 package com.testinium.trello.runners;
 
-public class TestRunner {}
+import io.cucumber.junit.platform.engine.Cucumber;
+
+/**
+ * This class enables Cucumber to be executed using the JUnit5 platform.
+ * JUnit5 discovers this test runner automatically during Maven Surefire execution.
+ * Cucumber features under src/test/resources/features will be executed as test cases.
+ */
+@Cucumber
+public class TestRunner {
+    // No code is required here.
+    // The @Cucumber annotation enables the JUnit5 platform engine for Cucumber.
+}
